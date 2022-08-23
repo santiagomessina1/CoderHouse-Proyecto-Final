4737614fab066406de5e3c5667541ae2962fd781
santimessina <santiagomessina1@gmail.com>
2022-08-23 18:21:21 -0300

    primer commit

---
+++ js/app.js
@@ -0,0 +1,234 @@
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const cartFooterDOM = document.querySelector(".cart-footer");
+const emptyCartFooterDOM = document.querySelector(".empty-cart-footer");
+const productsDOM = document.querySelector(".products-center");
+
+let cart = [];
+let buttonsDOM = [];
+
+
+// APLICANDO FETCH 
+const getProducts = async () => {
+    try {
+        let response = await fetch("products.json");
+        let data = await response.json();
+        let products = data;
+        return products;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const displayProducts = products => {
+    for (const product of products) {
+        let displayProducts = document.createElement("article");
+        displayProducts.className = "product";
+        displayProducts.innerHTML = `
+       
+        <div class="img-container" id="products">
+            <img src=${product.image} alt="Producto" class="product-img">
+            
+        </div>
+        <h3>${product.title}</h3>
+        <h4>$${product.price}
+        <button class="bag-btn" data-id=${product.id}>
+                <i class="fa-solid fa-cart-shopping"></i>
+                Agregar al carrito
+            </button>
+        </h4>
+        
+        `;
+        productsDOM.appendChild(displayProducts);
+    }
+}
+
+const getButtons = () => {
+    const buttons = [...document.querySelectorAll(".bag-btn")];
+    buttonsDOM = buttons; 
+    buttons.forEach(btn => {
+        let id = btn.dataset.id;
+        let inCart = cart.find(product => product.id == id);
+        if (inCart) {
+            btn.innerText = "¡Agregado!";
+            btn.disabled = true;
+        } 
+        btn.addEventListener("click", e => {
+            e.target.innerText = "Agregado";
+            e.target.disabled = true;
+            let cartItem = {...getProduct(id), amount: 1}; 
+            cart = [...cart, cartItem];
+            saveCart(cart);
+            setCartValues(cart);
+            addCartItem(cartItem);
+            showCart();
+        });
+    });
+}
+
+const setCartValues = cart => {
+    let tempTotal = 0;
+    let itemsTotal = 0; 
+    cart.map(item => {
+        tempTotal += item.price * item.amount;
+        itemsTotal += item.amount; 
+    });
+    cartTotal.innerText = tempTotal;
+    cartItems.innerText = itemsTotal;
+    if (cart.length <= 0) {
+        emptyCartFooterDOM.style.display = "block";
+        cartFooterDOM.style.display = "none";
+    } else {
+        emptyCartFooterDOM.style.display = "none";
+        cartFooterDOM.style.display = "block";
+    }
+}
+
+const addCartItem = item => {
+    let div = document.createElement("div");
+    div.className = "cart-item";
+    div.innerHTML = `
+    <img src="${item.image}"  alt="Producto" >
+    <div>
+        <h4>${item.title}</h4>
+        <h5>$${item.price}</h5>
+        <span class="remove-item" data-id=${item.id}>Eliminar</span>
+    </div>
+    <div>
+        <i class="fa-solid fa-chevron-up" data-id=${item.id}></i>
+        <p class="item-amount">${item.amount}</p>
+        <i class="fa-solid fa-chevron-down" data-id=${item.id}></i>
+    </div>
+    `;
+    cartContent.appendChild(div);
+}
+
+const showCart = () => {
+    cartOverlay.classList.add("transparent-bcg");
+    cartDOM.classList.add("show-cart");
+}
+
+const setupApp = () => {
+    cart = getCart();
+    setCartValues(cart);
+    populateCart(cart);
+    cartBtn.addEventListener("click", showCart);
+    closeCartBtn.addEventListener("click", hideCart);
+}
+
+const populateCart = cart => {
+    cart.forEach(item => addCartItem(item));
+}
+
+const hideCart = () => {
+    cartOverlay.classList.remove("transparent-bcg");
+    cartDOM.classList.remove("show-cart");
+}
+
+const cartLogic = () => {
+    clearCartBtn.addEventListener("click", () => {
+        swal({
+            title: "¿Estás seguro que querés vaciar tu carrito?",
+            buttons: {
+                cancel: {
+                    text: "Cancelar",
+                    value: false,
+                    visible: true
+                },
+                confirm: {
+                    text: "Sí, estoy seguro",
+                    value: true,
+                    visible: true,
+                    closeModal: true
+                }
+            }
+        }).then(willDelete => willDelete && clearCart());
+    });
+
+    cartContent.addEventListener("click", e => {
+        if (e.target.classList.contains("remove-item")) {
+            let removeCartItem = e.target;
+            let id = removeCartItem.dataset.id;
+            cartContent.removeChild(removeCartItem.parentElement.parentElement);
+            removeItem(id);
+        } else if (e.target.classList.contains("fa-chevron-up")) {
+            let addAmount = e.target;
+            let id = addAmount.dataset.id; 
+            let tempItem = cart.find(item => item.id == id);
+            tempItem.amount++; 
+            saveCart(cart);
+            setCartValues(cart);
+            addAmount.nextElementSibling.innerText = tempItem.amount;
+        } else if (e.target.classList.contains("fa-chevron-down")) {
+            let lowerAmount = e.target;
+            let id = lowerAmount.dataset.id; 
+            let tempItem = cart.find(item => item.id == id);
+            tempItem.amount--; 
+            if (tempItem.amount > 0) {
+                saveCart(cart);
+                setCartValues(cart);
+                lowerAmount.previousElementSibling.innerText = tempItem.amount;
+            } else {
+                cartContent.removeChild(lowerAmount.parentElement.parentElement);
+                removeItem(id);
+            }
+        }
+    });
+}
+
+const clearCart = () => {
+    let cartItems = cart.map(item => item.id);
+    cartItems.forEach(id => removeItem(id));
+    while (cartContent.children.length > 0) {
+        cartContent.removeChild(cartContent.children[0]);
+    }
+    hideCart();
+}
+
+const removeItem = id => {
+    cart = cart.filter(item => item.id != id);
+    setCartValues(cart);
+    saveCart(cart);
+    let button = getSingleButton(id);
+    button.disabled = false;
+    button.innerHTML = `<i class="fa-solid fa-cart-shopping"></i> Agregar al carrito`;
+}
+
+const getSingleButton = id => buttonsDOM.find(button => button.dataset.id == id);
+
+const saveProducts = products => {
+    localStorage.setItem("products", JSON.stringify(products));
+}
+
+const getProduct = id => {
+    products = JSON.parse(localStorage.getItem("products"));
+    return products.find(product => product.id == id);
+}
+
+const saveCart = cart => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+const getCart = () => localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : [];
+
+setupApp();
+
+getProducts().then(products => {
+    displayProducts(products);
+    saveProducts(products);
+}).then(() => {
+    getButtons();
+    cartLogic();
+});
+
+
+
+
+
+
---
+++ js/checkout.js
@@ -0,0 +1,235 @@
+//variables
+const inputs = [...document.querySelectorAll(".input")]; 
+const checkoutInfo = document.querySelector(".checkout-info");
+const checkoutTotal = document.querySelector(".checkout-total"); 
+const form = document.querySelector(".checkout-form");
+const email = document.querySelector("#email");
+const firstname = document.querySelector("#firstname");
+const lastname = document.querySelector("#lastname");
+const dni = document.querySelector("#dni");
+const phone = document.querySelector("#phone");
+const street = document.querySelector("#street");
+const number = document.querySelector("#number");
+
+let cartSummary = [];
+
+const getCartSummary = () => localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : [];
+
+const displayCartSummary = cartSummary => {
+    for (const item of cartSummary) {
+        let div = document.createElement("div");
+        div.className = "cart-item";
+        div.innerHTML = `
+        <img src="${item.image}" alt="Producto">
+        <div>
+            <h4>${item.title}</h4>
+            <h5>$${item.price}</h5>
+            <p>Cantidad: ${item.amount}</p>
+        </div>
+        `;
+        checkoutInfo.appendChild(div);
+    }
+}
+
+const setCheckoutTotal = cartSummary => {
+    let tempTotal = 0;
+    cartSummary.map(item => {
+        tempTotal += item.price * item.amount;
+    });
+    checkoutTotal.innerText = tempTotal;
+}
+
+const setupCheckout = () => {
+    cartSummary = getCartSummary();
+    displayCartSummary(cartSummary);
+    setCheckoutTotal(cartSummary);
+}
+
+//house or flat
+
+
+//check form validity
+const validateForm = () => {
+    form.noValidate = true;
+    form.addEventListener("submit", e => {
+        e.preventDefault();	
+        inputs.forEach(input => {
+            checkInputs(input);
+        });
+        if (form.checkValidity()) {
+            swal({
+                title: "Te estamos redirigiendo a Mercado Pago",
+                text: "¡Gracias por elegirnos!",
+                button: false
+            }).then(setTimeout(() => {
+                mercadopago();
+            }, 3000));
+        }
+    });
+    inputs.forEach(input => {
+        input.addEventListener("blur", () => {
+            checkInputs(input);
+        });
+    });
+}
+
+const checkInputs = input => {
+    //variables
+    const emailValue = email.value.trim();
+    const firstnameValue = firstname.value.trim();
+    const lastnameValue = lastname.value.trim();
+    const dniValue = dni.value.trim();
+    const phoneValue = phone.value.trim();
+    const streetValue = street.value.trim();
+    const numberValue = number.value.trim();
+
+    //regex
+    const lettersPattern = /^[A-ZÀ-Ú]+$/i;
+    const numbersPattern = /^[0-9]+$/;
+
+    //logic
+    switch (input) {
+        case email:
+            if (emailValue === "") {
+                setErrorFor(email, "Este campo es obligatorio.");
+            } else if (!isEmail(emailValue)) {
+                setErrorFor(email, "Introduzca un email válido.");
+            } else {
+                setSuccessFor(email);
+            }
+            break;
+
+        case firstname:
+            if (firstnameValue === "") {
+                setErrorFor(firstname, "Este campo es obligatorio.");
+            } else if (!lettersPattern.test(firstnameValue)) {
+                setErrorFor(firstname, "Introduzca un nombre válido.");
+            } else {
+                setSuccessFor(firstname);
+            }
+            break;
+
+        case lastname:
+            if (lastnameValue === "") {
+                setErrorFor(lastname, "Este campo es obligatorio.");
+            } else if (!lettersPattern.test(lastnameValue)) {
+                setErrorFor(lastname, "Introduzca un apellido válido.");
+            } else {
+                setSuccessFor(lastname);
+            }
+            break;
+
+        case dni:
+            if (dniValue === "") {
+                setErrorFor(dni, "Este campo es obligatorio.");
+            } else if ((!numbersPattern.test(dniValue)) || (dniValue.length < 6)) {
+                setErrorFor(dni, "Introduzca un documento válido.");
+            } else {
+                setSuccessFor(dni);
+            }
+            break;
+            
+        case phone:        
+            if (phoneValue === "") {
+                setErrorForPhone(phone, "Este campo es obligatorio.");
+            } else if ((!numbersPattern.test(phoneValue)) || (phoneValue.length < 8)) {
+                setErrorForPhone(phone, "Introduzca un celular válido.");
+            } else {
+                const formControl = phone.parentElement.parentElement;
+                formControl.className = "form-control success";
+            }
+            break;
+
+        case street:
+            if (streetValue === "") {
+                setErrorFor(street, "Este campo es obligatorio.");
+            } else if (!lettersPattern.test(streetValue)) {
+                setErrorFor(street, "Introduzca una calle válida.");
+            } else {
+                setSuccessFor(street);
+            }
+            break;
+
+        case number: 
+            if (numberValue === "") {
+                setErrorFor(number, "Este campo es obligatorio.");
+            } else if (!numbersPattern.test(numberValue)) {
+                setErrorFor(number, "Introduzca un número válido.");
+            } else {
+                setSuccessFor(number);
+            }
+            break;
+
+        default: 
+            console.log("Switch error");
+            break;
+    }
+}
+
+//validate email
+const isEmail = email => /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+
+//success-error messages
+const setErrorFor = (input, message) => {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small");
+    formControl.className = "form-control error";
+    small.innerText = message;
+}
+
+const setSuccessFor = input => {
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
+
+const setErrorForPhone = (input, message) => {
+    const formControl = input.parentElement.parentElement;
+    const small = formControl.querySelector("small");
+    formControl.className = "form-control error";
+    small.innerText = message;
+}
+
+//payment
+const mercadopago = async () => {
+    const cartSummaryToMap = cartSummary.map(item => {
+        let newItem =     
+        {
+            title: item.title,
+            description: "",
+            picture_url: item.image,
+            category_id: item.id,
+            quantity: item.amount,
+            currency_id: "ARS",
+            unit_price: item.price
+        }
+        return newItem;
+    });
+
+    try {
+        let response = await fetch("https://api.mercadopago.com/checkout/preferences", {
+            method: "POST",
+            headers: {
+                Authorization: "TEST-440207383957120-081711-fc59223c52fe00af81bc653e4502d886-396230180"
+            },
+            body: JSON.stringify({
+                items: cartSummaryToMap,
+                back_urls: {
+                    "success": "http://127.0.0.1:5500/index.html",
+                    "failure": "http://127.0.0.1:5500/index.html",
+                    "pending": "http://127.0.0.1:5500/index.html"
+                },
+                auto_return: "approved"
+            })
+        });
+        let data = await response.json();
+        window.open(data.init_point, "_self");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//execution
+setupCheckout();
+validateForm();
+
+
